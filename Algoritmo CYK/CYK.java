b4d92dd4193c39ff4e6f2335f28f9d4bec6748af
Mario Navarete <marionb@ciencias.unam.mx>
2019-05-08 11:25:19 -0500

    Add files via upload

--- Algoritmo CYK/CYK.java
+++ Algoritmo CYK/CYK.java
@@ -1,3 +1,31 @@
+/* -------------------------------------------------------------------
+ * CYK.java
+ * versión 1.0
+ * Copyright (C) 2019  Mario Navarrete Baltazar.
+ * Facultad de Ciencias,
+ * Universidad Nacional Autónoma de México, Mexico.
+ *
+ * Este programa es software libre; se puede redistribuir
+ * y/o modificar en los términos establecidos por la
+ * Licencia Pública General de GNU tal como fue publicada
+ * por la Free Software Foundation en la versión 2 o
+ * superior.
+ *
+ * Este programa es distribuido con la esperanza de que
+ * resulte de utilidad, pero SIN GARANTÍA ALGUNA; de hecho
+ * sin la garantía implícita de COMERCIALIZACIÓN o
+ * ADECUACIÓN PARA PROPÓSITOS PARTICULARES. Véase la
+ * Licencia Pública General de GNU para mayores detalles.
+ *
+ * Con este programa se debe haber recibido una copia de la
+ * Licencia Pública General de GNU, de no ser así, visite el
+ * siguiente URL:
+ * http://www.gnu.org/licenses/gpl.html
+ * o escriba a la Free Software Foundation Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * -------------------------------------------------------------------
+*/
+
 import java.io.*;
 import java.util.*;
 
